890210159c65c26cf50c1ac663381ee72d3fb498
Name <Name@DESKTOP-6ADNBQH>
2020-11-07 19:07:32 +0100

    Ver. 0.4 Main window

--- RoomBooking.Wpf/ViewModels/MainViewModel.cs
+++ RoomBooking.Wpf/ViewModels/MainViewModel.cs
@@ -1,33 +1,60 @@
-﻿using RoomBooking.Wpf.Common;
+﻿using RoomBooking.Core.Contracts;
+using RoomBooking.Core.Entities;
+using RoomBooking.Persistence;
+using RoomBooking.Wpf.Common;
 using RoomBooking.Wpf.Common.Contracts;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RoomBooking.Wpf.ViewModels
 {
-  public class MainViewModel : BaseViewModel
-  {
-    public MainViewModel(IWindowController windowController) : base(windowController)
+    public class MainViewModel : BaseViewModel
     {
-    }
+        private ObservableCollection<Booking> _bookings;
 
-    private Task LoadDataAsync()
-    {
-      throw new NotImplementedException();
-    }
+        public ObservableCollection<Booking> Bookings 
+        {
+            get { return _bookings; }
+            set
+            {
+                _bookings = value;
+                OnPropertyChanged(nameof(Bookings));
+                Validate();
+            }
+        }
 
-    public static async Task<MainViewModel> CreateAsync(IWindowController windowController)
-    {
-      var viewModel = new MainViewModel(windowController);
-      await viewModel.LoadDataAsync();
-      return viewModel;
-    }
+        public MainViewModel(IWindowController windowController) : base(windowController)
+        {
+            Validate();
+        }
 
-    public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-    {
-      throw new NotImplementedException();
+        private async Task LoadDataAsync()
+        {
+            using IUnitOfWork unitOfWork = new UnitOfWork();
+            var bookings = (await unitOfWork.Bookings
+                .GetAllAsync())
+                .ToList()
+                ;
+            Bookings = new ObservableCollection<Booking>(bookings);
+        }
+
+        public static async Task<MainViewModel> CreateAsync(IWindowController windowController)
+        {
+            var viewModel = new MainViewModel(windowController);
+            await viewModel.LoadDataAsync();
+            return viewModel;
+        }
+
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Bookings == null)
+            {
+                yield return new ValidationResult($"Datenbank ist fehlerhaft", new string[] { nameof(Bookings) });
+            }
+        }
     }
-  }
 }
